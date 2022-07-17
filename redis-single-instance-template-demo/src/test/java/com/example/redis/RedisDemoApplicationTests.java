d1bef9a6c1216be11ec33b6d2c4bfb7d3b8da259
hakantaylan <hakantaylan@users.noreply.github.com>
2022-07-17 16:58:23 +0300

    new examples added

--- redis-single-instance-template-demo/src/test/java/com/example/redis/RedisDemoApplicationTests.java
+++ redis-single-instance-template-demo/src/test/java/com/example/redis/RedisDemoApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.redis;
 
+import example.springdata.redis.test.condition.EnabledOnRedisAvailable;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnabledOnRedisAvailable
 class RedisDemoApplicationTests {
 
 	@Test
